164c8f40a343d48a9a74310730d02bde42a08bcf
Brian <chumobrian@gmail.com>
2024-04-19 02:15:44 +0300

    0x00-python_variable_annotations tasks complete

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,5 +1,4 @@
 #!/usr/bin/env python3
-import math
 """
    Defines a module that implements a function floor which takes
    a float n as argument and returns the floor of the float
@@ -14,4 +13,4 @@ def floor(n: float) -> int:
     Returns:
         int: The floor of `n`.
     """
-    return math.floor(n)
+    return int(n)
--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -3,6 +3,7 @@
     Defines a module with annotated variables
 """
 
+
 a: int = 1
 pi: float = 3.14
 i_understand_annotations: bool = True
