0c7f6a48a48c063c5c2bdf5ee89dc40a355cb3fd
Brian <chumobrian@gmail.com>
2024-04-19 02:42:54 +0300

    feat: Add an annotation of the function with appropriate types

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,5 +1,10 @@
-# The types of the elements of the input are not know
-def safe_first_element(lst):
+#!/usr/bin/env python3
+'''Defines a module with an annotated function and values'''
+from typing import Iterable, List, Sequence, Any, Union
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    '''Returns None values'''
     if lst:
         return lst[0]
     else:
