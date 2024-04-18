2fa22e76e26673705ad56a75765e6233fd8fe633
Brian <chumobrian@gmail.com>
2024-04-18 17:56:49 +0300

    feat: Add an annotation of the function with appropriate types

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,2 +1,18 @@
-def element_length(lst):
-    return [(i, len(i)) for i in lst
+#!/usr/bin/env python3
+'''Defines a module with an annotated function and values'''
+
+from typing import Iterable, List, Sequence, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Returns a tuple.
+
+    Args:
+        lst (Iterable[Sequence]): The input iterable containing sequences.
+
+    Returns:
+        List[Tuple[Sequence, int]]: A list of tuples where each containg an
+        element from the input list and its corresponding length.
+    """
+    return [(i, len(i)) for i in lst]
