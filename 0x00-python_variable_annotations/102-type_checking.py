efcfeff19f248bad6ca5708df399efbd3e1fca72
Brian <chumobrian@gmail.com>
2024-04-19 03:35:28 +0300

    feat: Add an annotation of the function with appropriate types

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -1,5 +1,12 @@
-def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
-    zoomed_in: Tuple = [
+#!/usr/bin/env python3
+'''Defines a module with an annotated function and values'''
+
+from typing import Tuple, List, Any
+
+
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''Returns a list of ints'''
+    zoomed_in: List = [
         item for item in lst
         for i in range(factor)
     ]
@@ -8,6 +15,6 @@ def zoom_array(lst: Tuple, factor: int = 2) -> Tuple:
 
 array = [12, 72, 91]
 
-zoom_2x = zoom_array(array)
+zoom_2x = zoom_array(tuple(array))
 
-zoom_3x = zoom_array(array, 3.0)
+zoom_3x = zoom_array(tuple(array), int(3.0))
