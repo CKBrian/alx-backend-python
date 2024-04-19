e9fd741149aedc081287a20548c60a62fa543c1f
Brian <chumobrian@gmail.com>
2024-04-19 03:18:17 +0300

    feat: Add an annotation of the function with appropriate types

--- 0x00-python_variable_annotations/101-safely_get_value.py
+++ 0x00-python_variable_annotations/101-safely_get_value.py
@@ -1,4 +1,22 @@
-def safely_get_value(dct, key, default = None):
+#!/usr/bin/env python3
+'''Defines a module with an annotated function and values'''
+from typing import Mapping, Any, Union, TypeVar
+
+
+T = TypeVar('T')
+
+
+def safely_get_value(dct: Mapping, key: Any,
+                     default: Union[T, None] = None) -> Union[Any, T]:
+    '''
+    Returns a dct element or default value.
+
+    Args:
+        dst (Mapping): The input Mapping.
+
+    Returns:
+        [Any | T]: A default or a dct item.
+    '''
     if key in dct:
         return dct[key]
     else:
