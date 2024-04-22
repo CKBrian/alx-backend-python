10306fe750718ca30a1b55071259235c4a9fbb28
Brian <chumobrian@gmail.com>
2024-04-22 21:03:24 +0300

    feat: Add an asynchronous coroutine that takes in an integer argument, waits for a random delay between 0 and max_delay and eventually returns it.

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+'''
+    Defines a module with an asynchronous coroutine that
+    takes in an integer argument that waits for a random delay
+    between 0 and max_delay seconds and eventually returns it
+'''
+
+import asyncio
+import random
+
+
+async def wait_random(max_delay=10):
+    '''
+        Returns an integer after a delay
+
+        Args:
+            max_delay (int): An integer number
+
+        Returns:
+            int : max_delay's value
+    '''
+    delay2 = random.uniform(0, max_delay)
+    await asyncio.sleep(delay2)
+    return delay2
