09299dfaf74c28dabd977a10ffb1214880d551dd
Brian <chumobrian@gmail.com>
2024-04-22 23:03:38 +0300

    feat: Add a function that returns a task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,7 +7,7 @@ from typing import Awaitable
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable[float]:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     '''return a asyncio.Task'''
     task = asyncio.create_task(wait_random(max_delay))
     return task
