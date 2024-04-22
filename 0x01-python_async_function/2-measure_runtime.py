bb24f02e83071e96309897a0c1a6cf4481d6d7a1
Brian <chumobrian@gmail.com>
2024-04-23 00:25:10 +0300

    Feat: Add an asyn function measure_time that measures     the total execution time for a wait_n() function and returns total_time / n

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 '''
-    Defines a module with an asyn function measure_time that measures
+    Defines a module with an async function measure_time that measures
     the total execution time for a wait_n() function and returns total_time / n
 '''
 
@@ -9,8 +9,9 @@ import time
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int) -> float:
+def measure_time(n: int, max_delay: int) -> float:
     '''Returns the total execution time'''
-    p = time.perf_counter()
-    await wait_n(n, max_delay)
-    return (time.perf_counter() - p) / n
+    initial = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    elapse = time.time() - initial
+    return elapse / n
