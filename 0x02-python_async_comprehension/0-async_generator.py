89325a778370f69ea31df8b4977049e19273207a
Brian <chumobrian@gmail.com>
2024-04-23 17:17:22 +0300

    feat: Add an asynchronous function async_generator

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -1,11 +1,12 @@
-#/usr/bin/env python3
+#!/usr/bin/env python3
 '''Defines a module with an asynchronous function async_generator'''
 
 import asyncio
 import random
+from typing import AsyncGenerator
 
 
-async def async_generator():
+async def async_generator() -> AsyncGenerator[float]:
     """Yields a random number between 0 and 10"""
     for _ in range(10):
         yield random.uniform(0, 10)
