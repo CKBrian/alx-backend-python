7c2793ebc8ea42b3313f95c6edae37d8c9fb6f12
Brian <chumobrian@gmail.com>
2024-05-13 18:36:44 +0300

    Feat: Add a unit tesst for testing the utils module

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,15 +1,25 @@
 #!/usr/bin/env/ python3
 '''
-   	Defines a test_utils module with a TestAccessNestedMap class
+       Defines a test_utils module with a TestAccessNestedMap class
 '''
 
 import unittest
+from parameterized import parameterized
+from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-	'''Defines a TestAccessNestedMap unit testing class'''
-    pass
+    '''Defines a TestAccessNestedMap unit testing class'''
+    @parameterized.expand([
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), 2),
+            ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
 
+    def test_access_nested_map(self, nested_map, path, expected):
+        '''tests the utils access_nested_map method'''
+        res = access_nested_map(nested_map, path)
+        self.assertEqual(res, expected)
 
 if __name__ == '__main__':
     unittest.main()
