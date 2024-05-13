137c06315f0dddeb3724d8f616766149b8a288ba
Brian <chumobrian@gmail.com>
2024-05-13 15:07:43 +0300

    Chore: Add scripts with function for unit and intergration testing project tasks

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env/ python3
+'''
+   	Defines a test_utils module with a TestAccessNestedMap class
+'''
+
+import unittest
+
+
+class TestAccessNestedMap(unittest.TestCase):
+	'''Defines a TestAccessNestedMap unit testing class'''
+    pass
+
+
+if __name__ == '__main__':
+    unittest.main()
