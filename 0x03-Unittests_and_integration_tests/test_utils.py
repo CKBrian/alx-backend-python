0128f81e3d0d217d4c878cf81637d2b098f16506
Brian <chumobrian@gmail.com>
2024-05-13 19:05:02 +0300

    Feat: Add a unit tesst for testing the utils module

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -22,7 +22,10 @@ class TestAccessNestedMap(unittest.TestCase):
             ({"a": {"b": 2}}, ("a",), {'b': 2}),
             ({"a": {"b": 2}}, ("a", "b"), 2)
     ])
-    def test_access_nested_map(self, nested_map: Mapping, path: Sequence, expected: Any) -> None:
+    def test_access_nested_map(self,
+                               nested_map: Mapping,
+                               path: Sequence,
+                               expected: Any) -> None:
         '''tests the utils access_nested_map method'''
         res = access_nested_map(nested_map, path)
         self.assertEqual(res, expected)
